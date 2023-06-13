ce7d8cd8340fd0ef2f618379cf2ef0fe0c30dad8
belem <graceitamunoala@gmail.com>
2023-06-13 20:00:23 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Hello></Hello>
-      
+      <Hello/>
+      <Message/>
+
     </div>
   );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,15 @@
-function Hello() {
-    return <h1>Hello World!</h1>
+/*function Add() {
+    return 10 + 2;
+}*/
+
+const Add = ()=> {
+    return 10 + 2;
 }
+
+function Hello() {
+    return <h1>10 + 2 = {Add()}!</h1>
+}
+
+//const Hello = ()=> <h1>Hello There</h1>
 
 export default Hello;
